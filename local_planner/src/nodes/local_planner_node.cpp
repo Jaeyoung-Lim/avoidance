3b8542416a1399f335a16db13d2158588e737a7b
JaeyoungLim <gokoreas@gmail.com>
2019-03-29 21:43:15 +0100

    Include exit signals in destructor

--- local_planner/src/nodes/local_planner_node.cpp
+++ local_planner/src/nodes/local_planner_node.cpp
@@ -113,6 +113,9 @@ LocalPlannerNode::LocalPlannerNode(const ros::NodeHandle& nh,
 }
 
 LocalPlannerNode::~LocalPlannerNode() {
+  should_exit_ = true;
+  data_ready_cv_.notify_all();
+
   delete server_;
   delete tf_listener_;
 }
